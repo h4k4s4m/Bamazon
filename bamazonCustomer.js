447f8b76750d0a92c63380c8ff546d8b180477b5
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-11-28 21:26:18 -0800

    zo get off get off

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,11 +13,37 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+displayStore();
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-});
+function displayStore() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("ITEMS");
+        console.log("_____________________________________________________________________");
+        for (var x in res) {
+            console.log(res[x].item_id + " : " + res[x].product_name + " : $" + res[x].price);
+        }
+        console.log("_____________________________________________________________________\n\n\n");
+        buy();
+    });
+}
 
 
-connection.end();
+function buy() {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "ID",
+            message: "What is the ID of the item you would like to buy?"
+        }])
+        .then((response) => {
+            connection.query("SELECT * FROM products WHERE ?", {
+                item_id: response.ID
+            }, function (err, res) {
+                if (err) throw err;
+                console.log(res);
+                connection.end();
+            });
+        });
+
+}
